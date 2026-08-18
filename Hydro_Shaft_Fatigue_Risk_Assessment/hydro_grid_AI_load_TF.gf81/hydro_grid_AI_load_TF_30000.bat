91042db9db98e3ad1a9e5cb0ad7b1e541755dbc1
antoscvarghese-pnnl <antoscheeramban.varghese@pnnl.gov>
2026-08-18 09:13:52 -0700

    Uploading PSCAD file with play back capability

---
+++ Hydro_Shaft_Fatigue_Risk_Assessment/hydro_grid_AI_load_TF.gf81/hydro_grid_AI_load_TF_30000.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files\GFortran\8.1\x64\bin\gf81vars.bat"
+pushd "C:\Users\varg349\OneDrive - PNNL\Antos_Files\2. GridODA - WPTO\Public_Git_Hydro_WPTO\Hydro-Oscillations\Hydro_Shaft_Fatigue_Risk_Assessment\hydro_grid_AI_load_TF.gf81\"
+hydro_grid_AI_load_TF.exe -v5 -config hydro_grid_AI_load_TF_1.config
+popd
